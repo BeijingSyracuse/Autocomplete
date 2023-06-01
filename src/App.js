9fe294d1e6578dbc3c40e49ce7a875ddf30cda97
BeijingSyracuse <70109739+BeijingSyracuse@users.noreply.github.com>
2023-06-01 15:45:14 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,106 @@
+import { useState } from "react";
+
+const fruit = [
+  "Apple",
+  "Apricot",
+  "Avocado ",
+  "Banana",
+  "Bilberry",
+  "Blackberry",
+  "Blackcurrant",
+  "Blueberry",
+  "Boysenberry",
+  "Currant",
+  "Cherry",
+  "Coconut",
+  "Cranberry",
+  "Cucumber",
+  "Custard apple",
+  "Damson",
+  "Date",
+  "Dragonfruit",
+  "Durian",
+  "Elderberry",
+  "Feijoa",
+  "Fig",
+  "Gooseberry",
+  "Grape",
+  "Raisin",
+  "Grapefruit",
+  "Guava",
+  "Honeyberry",
+  "Huckleberry",
+  "Jabuticaba",
+  "Jackfruit",
+  "Jambul",
+  "Juniper berry",
+  "Kiwifruit",
+  "Kumquat",
+  "Lemon",
+  "Lime",
+  "Loquat",
+  "Longan",
+  "Lychee",
+  "Mango",
+  "Mangosteen",
+  "Marionberry",
+  "Melon",
+  "Cantaloupe",
+  "Honeydew",
+  "Watermelon",
+  "Miracle fruit",
+  "Mulberry",
+  "Nectarine",
+  "Nance",
+  "Olive",
+  "Orange",
+  "Clementine",
+  "Mandarine",
+  "Tangerine",
+  "Papaya",
+  "Passionfruit",
+  "Peach",
+  "Pear",
+  "Persimmon",
+  "Plantain",
+  "Plum",
+  "Pineapple",
+  "Pomegranate",
+  "Pomelo",
+  "Quince",
+  "Raspberry",
+  "Salmonberry",
+  "Rambutan",
+  "Redcurrant",
+  "Salak",
+  "Satsuma",
+  "Soursop",
+  "Star fruit",
+  "Strawberry",
+  "Tamarillo",
+  "Tamarind",
+  "Yuzu"
+];
+
+let sorted = fruit.sort((a, b) => b.localeCompare(a));
+
+export default function App() {
+  const [list, setList] = useState(fruit);
+
+  const handleInput = (e) => {
+    setList(
+      sorted.filter((ele) =>
+        ele.toLowerCase().includes(e.target.value.toLowerCase())
+      )
+    );
+  };
+  // 加sensitive，tolowercase
+  //加sorted前   fruit.filter((ele)  b.localeCompare(a)倒序
+
+  return (
+    <div className="App">
+      <input type="text" placeholder="search" onChange={handleInput} />
+      <ul>{list && list.map((item, index) => <li key={index}>{item}</li>)}</ul>
+    </div>
+  );
+}
